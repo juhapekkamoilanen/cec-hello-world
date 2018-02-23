f96c3a1c7d41774e19d663a9bb6b0bc1a365bbe7
Juha-Pekka Moilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-23 09:47:58 +0200

    Test code change

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    return "Hello CEC, Im JPM! Greetings from "+socket.gethostname()+"\n"
 
 
 if __name__ == "__main__":
