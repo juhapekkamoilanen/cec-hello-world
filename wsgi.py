cd1815a357a867445026da0fa48e68c3805e3c63
Juha-Pekka Moilanen <juha-pekka.moilanen@helsinki.fi>
2018-02-23 10:31:36 +0200

    Add log writer

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,13 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     print('hello you log watcher!')
-    return "Hello CEC, Im JPM! Greetings from "+socket.gethostname()+"\n"
+    
+    file_obj = open('/mnt/vanilla-logdir/cec-vanilla.log', 'a')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    file_obj.write(timestamp + ' ' + socket.gethostname() + ' \n')
+    file_obj.close()
+    
+    return "Hello CEC, Im JPM - 2! Greetings from "+socket.gethostname()+"\n"
 
 
 if __name__ == "__main__":
